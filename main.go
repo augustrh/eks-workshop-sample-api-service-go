74c459f5a0cbf2e05e3fa34172e11096945a2714
augustrh <asimonel@redhat.com>
2021-09-10 13:45:34 +1000

    test spec again

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World From Me!!!!!!"}
+		res := &response{Message: "hello"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
